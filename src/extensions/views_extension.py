f0f3e1b7337f1668276cabe9b9d7429e92172c40
stevenq9 <steven.quispe@epn.edu.ec>
2024-01-08 23:10:54 -0500

    fix(app): Added delete packages logic

--- src/extensions/views_extension.py
+++ src/extensions/views_extension.py
@@ -4,6 +4,7 @@ from src.web.views.package_detail_view import PackageDetailView
 from src.web.views.package_add_manager import PackageAddView
 from web.views.package_edit_manager import PackageEditView
 from web.views.package_search_view import PackageSearchView
+from web.views.package_delete_view import PackageDeleteView
 
 
 def register_views(app):
@@ -12,5 +13,5 @@ def register_views(app):
     app.add_url_rule('/package_detail/<string:name>', view_func=PackageDetailView.as_view('package_detail'))
     app.add_url_rule('/edit_package/<string:name>', view_func=PackageEditView.as_view('edit_package'))
     app.add_url_rule('/addPackage', view_func=PackageAddView.as_view('add_package'))
+    app.add_url_rule('/deletePackage/<string:name>', view_func=PackageDeleteView.as_view('delete_package'))
     app.add_url_rule('/package_search', view_func=PackageSearchView.as_view('package_search'))
-
