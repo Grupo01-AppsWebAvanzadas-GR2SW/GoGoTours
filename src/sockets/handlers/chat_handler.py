01b4f3d96b0179aa0585195305727f172a4dd77b
Andrés Ponce <75549856+aPonce2001@users.noreply.github.com>
2024-01-11 07:33:41 -0500

    init(chat): begin chat socket

--- src/sockets/handlers/chat_handler.py
+++ src/sockets/handlers/chat_handler.py
@@ -8,10 +8,9 @@ from src.extensions.decorations_extension import login_required
 
 def register_chats_handlers(socketio: AsyncServer):
     @socketio.on('join_room')
-    @login_required
     @inject
-    def on_join_room(conversation_id: str):
-        user_id = session['id']
+    def on_join_room(socket: AsyncServer, conversation_id: str):
+        print(f"Joining room {conversation_id}")
         join_room(conversation_id)
 
     @socketio.on('send_message')
